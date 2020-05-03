530d1ab1241d43be4c8b0d0547b5426946fb2e10
Nitin Kumavat <nitin.s.kumavat@gmail.com>
2020-05-03 18:22:33 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/ticket-service-api',{
+mongoose.connect('mongodb://127.0.0.1:27017/ticket-service-api',{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true ,
